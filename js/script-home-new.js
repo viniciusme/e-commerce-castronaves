673b6c8452e055af41b898e41321dcdbee216570
Vinicius Mendes <vinimendesdev@gmail.com>
2023-08-03 17:20:20 -0300

    feat: added category carousel menu

--- js/script-home-new.js
+++ js/script-home-new.js
@@ -15,3 +15,30 @@ function removeAttributes(attributeName) {
     elementRemove.removeAttribute('height');
   }
 }
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    stagePadding: 40,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      500: {
+        items: 4,
+      },
+      700: {
+        items: 5,
+      },
+      900: {
+        items: 6,
+      },
+    },
+  });
+});
